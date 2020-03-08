bd54b56657838b59eb19c6f89b1b9fbe863979c1
Kyle Eggleston <kyle@dreamsense.com>
2020-03-08 12:14:33 -0600

    Added ticker content to java class

--- DreamSense.java
+++ DreamSense.java
@@ -4,7 +4,9 @@ import java.io.IOException;
 public class DreamSense {
 
   private static String index;
+  private static String tickercontent;
   private static StringBuilder sb;
+  private static StringBuilder quotesSB;
   private static String NL = "\n";
 
   public DreamSense() {
@@ -13,7 +15,9 @@ public class DreamSense {
 
   public void init() {
     index = "ds.html";
+    tickercontent = "tickercontent.txt";
     sb = new StringBuilder();
+    quotesSB = new StringBuilder();
   }
 
   private String[] poetry = {
@@ -32,6 +36,43 @@ public class DreamSense {
     "What's going on in itself",
     "Simplification and grasping for straws",
     "We all end up here somewhere"
+  };
+
+  private String[] quotes = {
+    "Never forget who you are and what you can do.",
+    "Look before you leap.",
+    "Not all simple solutions are the right solution.",
+    "Don't let your heart get in the way of your brain.",
+    "If you don't think something matters, think again.",
+    "Before you react, think to yourself one thing. Is it really worth it?",
+    "If something feels like it's too good to be true, it most likely is.",
+    "Follow your gut.",
+    "Admitting you're wrong is not a sign of weakness.",
+    "If someone is talking, don't interrupt them.",
+    "It's not over until you're dead, and even then it's not over.",
+    "The moment you take to look behind you is a second taken away from the end goal.",
+    "Don't act differently when you think no one is watching.",
+    "Keep track of your good days.",
+    "If you keep telling yourself you aren't good enough, you will eventually believe it.",
+    "There is no backspace key or white out when it comes to life. There is a learn and move forward option.",
+    "Try your hardest. Try your best.",
+    "Sometimes the bad guy gets away.",
+    "Life doesn't have a pause button.",
+    "If you think you are better than someone else, think again.",
+    "Don't be a lemming.",
+    "If you have something to say, say it. Before it's too late.",
+    "Don't laugh at the wrong time. You'll only look like a fool.",
+    "Selfish desires rarely lead to gold.",
+    "Don't dance with the devil.",
+    "You can't snooze through life.",
+    "Being obsessed doesn't make you right.",
+    "Don't say you're sorry when you have nothing to be sorry for.",
+    "A conversation with yourself can lead to confusion.",
+    "Never surrender if the cause is righteous.",
+    "Fight for what you believe in.",
+    "Sometimes you have to explain something over again until it sticks!",
+    "Be positive. Think happy thoughts.",
+    "Let's not dwell on all the things meant to be seen as stupid mediocrity."
   };
 
   public String createIndex() {
@@ -60,6 +101,10 @@ public class DreamSense {
     return "<" + element + ">" + t + "</" + element + ">";
   }
 
+  public String text(String element, String clazz, String t) {
+    return "<" + element + " class=\"" + clazz + "\">" + t + "</" + element + ">";
+  }
+
   public String elem(String element) {
     return "<" + element + "/>";
   }
@@ -68,7 +113,18 @@ public class DreamSense {
     return "<a href=\"" + page + "\">" + text + "</a>";
   }
 
-  public void writeIndex(String filename, String content) {
+  public String createTickerContent() {
+    quotesSB.append("<div>");
+    for (String line : quotes) {
+      quotesSB.append(text("div", "message", line));
+      quotesSB.append("\n");
+    }
+    quotesSB.append("</div>");
+
+    return quotesSB.toString();
+  }
+
+  public void writeFile(String filename, String content) {
     try {
       FileWriter myWriter = new FileWriter(filename);
       myWriter.write(content);
@@ -84,6 +140,9 @@ public class DreamSense {
     DreamSense ds = new DreamSense();
     ds.init();
     String idx = ds.createIndex();
-    ds.writeIndex(index, idx);
+    ds.writeFile(index, idx);
+
+    String q = ds.createTickerContent();
+    ds.writeFile(tickercontent, q);
   }
 }
